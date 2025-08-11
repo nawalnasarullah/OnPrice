ad557fef07dd283c89bcd230b0d14f3e5eaa9857
Nawal Nasarullah <nawalisbd@gmail.com>
2025-08-11 20:59:48 +0500

    "Updated allow_origins in CORSMiddleware to allow value '*,' in two places."

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ async def startup_event():
 # Equivalent to app.use(cors());
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change in production
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
